cf7595852921e0623a1e5a1f1687e436215e7995
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-07-25 22:55:56 -0300

    inicianto a refatoração para constructor functions

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 function createCalculator() {
    return {
       display: document.querySelector('.display'),
@@ -26,59 +27,114 @@ function createCalculator() {
       },
 
       startCalculation() {
-         let conta = this.display.value;
+         let calc= this.display.value;
          try {
-            conta = eval(conta);
+            calc= eval(conta);
 
-            /* 
+            /*
             aprendam como arrumar um bug XD
             if (!conta) {
-                alert('Conta inválida, geralmente elas são feitas usando números!')
+                alert('calcinválida, geralmente elas são feitas usando números!')
                 return;
              }*/
 
-            this.display.value = conta;
-         } catch (erro) {
-            alert('Conta inválida, geralmente elas são feitas usando números!')
-            return;
-         }
-      },
+/*        this.display.value = conta;
+     } catch (erro) {
+        alert('calcinválida, geralmente elas são feitas usando números!')
+        return;
+     }
+  },
 
-      clearDisplay() {
-         this.display.value = '';
-      },
-
-
-      deleteCharacter() {
-         this.display.value = this.display.value.slice(0, -1)
-      },
-
-      clickBtn() {
-         document.addEventListener('click', (event) => { // com arrow functions o this sempre sera o objeto que criou o elemento, nesse caso o this sera a calculator
-            const element = event.target;
-            if (element.classList.contains('btn-num')) {
-               this.enterValuesInDisplay(element.innerText);
-            };
-
-            if (element.classList.contains('btn-clear')) {
-               this.clearDisplay();
-            };
-
-            if (element.classList.contains('btn-del')) {
-               this.deleteCharacter();
-            };
-
-            if (element.classList.contains('btn-eq')) {
-               this.startCalculation();
-            };
-         })
-      },
+  clearDisplay() {
+     this.display.value = '';
+  },
 
 
-      enterValuesInDisplay(valor) {
-         this.display.value += valor;
-      },
-   }
+  deleteCharacter() {
+     this.display.value = this.display.value.slice(0, -1)
+  },
+
+  clickBtn() {
+     document.addEventListener('click', (event) => { // com arrow functions o this sempre sera o objeto que criou o elemento, nesse caso o this sera a calculator
+        const element = event.target;
+        if (element.classList.contains('btn-num')) {
+           this.enterValuesInDisplay(element.innerText);
+        };
+
+        if (element.classList.contains('btn-clear')) {
+           this.clearDisplay();
+        };
+
+        if (element.classList.contains('btn-del')) {
+           this.deleteCharacter();
+        };
+
+        if (element.classList.contains('btn-eq')) {
+           this.startCalculation();
+        };
+     })
+  },
+
+
+  enterValuesInDisplay(valor) {
+     this.display.value += valor;
+  },
+}
 }
 const calculator = createCalculator();
 calculator.startCalculator();
+*/
+//refazendo com constructor functions
+function Calculator() {
+   this.display = document.querySelector('.display');
+
+   this.eventClicks = () => {
+      document.addEventListener('click', event => {
+         const element = event.target;
+
+         if (element.classList.contains('btn-num')) {
+            this.addNumDisplay(element)
+         }
+
+         if (element.classList.contains('btn-clear')) {
+            this.clear()
+         }
+
+         if (element.classList.contains('btn-del')) {
+            this.del()
+         }
+
+         if (element.classList.contains('btn-eq')) {
+            this.initCalc()
+         }
+      });
+   };
+
+   this.clear = () => {
+      this.display.value = "";
+   };
+
+   this.addNumDisplay = (element) => {
+      this.display.value += element.innerText;
+   };
+
+   this.del = () => {
+      this.display.value = this.display.value.slice(0, -1)
+   };
+
+   this.pressDelete = () => {
+      this.display.addEventListener('keyup', (event) => {
+         if (event.keyCode === 46) {
+            this.clear();
+         }
+      })
+   };
+
+   this.initApp = () => {
+      this.eventClicks();
+      this.pressDelete();
+   };
+}
+
+const calculator = new Calculator();
+calculator.initApp();
